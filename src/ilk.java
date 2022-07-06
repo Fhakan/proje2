d40c6be8d6e14084db332ed606f15b4ac3aebd43
ozyur <ozyurtcihan@gmail.com>
2022-07-06 21:26:27 +0300

    cihan'ın kod satırı eklendi

--- src/ilk.java
+++ src/ilk.java
@@ -7,5 +7,6 @@ public class ilk {
         System.out.println("metin deneme");
         System.out.println("metin master deneme");
         System.out.println("Ertugrul deneme2");
+        System.out.println("cihan kod satırı");
     }
 }
